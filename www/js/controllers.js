5048335d615d3c76af28106adfe87fa5d6e24d55
Mahesh Sasidharan <mahesh.sasidharan.1990@gmail.com>
2016-10-23 20:20:07 -0500

    Fixed all views, removed unnecessary files, restructured code, bound using Angular objects in Results and Compare pages

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,135 @@
-angular.module('starter.controllers', [])
+kompair
+    .controller('HomeCtrl', ['sharedProperties', HomeCtrl])
+    .controller('ResultsCtrl', ['sharedProperties', 'CommonRoutines', ResultsCtrl])
+    .controller('CompairCtrl', ['sharedProperties', CompairCtrl])
+    .controller('MainCtrl', ['$scope', 'sharedProperties', MainCtrl]);
 
-.controller('DashCtrl', function($scope) {})
+function HomeCtrl(sharedProperties) {
+    var vm = this;
+    vm.oShared = sharedProperties;
+    vm.oShared.bSingedIn = false;
+    vm.SignIn = function() {
+        vm.oShared.bSingedIn = true;
+    }
+}
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+function ResultsCtrl(sharedProperties, CommonRoutines) {
+    var res = this;
+    res.arrResults = [{
+        id: 1,
+        stars: 4,
+        answers: 2,
+        views: 36,
+        title: 'Apple vs Orange',
+        arrCategories: ['Food', 'Natural']
+    }, {
+        id: 2,
+        stars: 4,
+        answers: 9,
+        views: 219,
+        title: 'Apple vs Android',
+        arrCategories: ['Tech', 'Artificial']
+    }, {
+        id: 3,
+        stars: 0,
+        answers: 0,
+        views: 3,
+        title: 'Blue vs Orange',
+        arrCategories: ['Color', 'Natural']
+    }, {
+        id: 4,
+        stars: 7,
+        answers: 14,
+        views: 623,
+        title: 'Hillary vs Orange',
+        arrCategories: ['Politics']
+    }];
+    res.oCompair = {
+        id: 1,
+        stars: 4,
+        answers: 2,
+        views: 36,
+        title: 'Apple vs Orange',
+        arrCategories: ['Food', 'Natural'],
+        thumbsUp: 10,
+        thumbsDown: 20,
+        oCompairItem: [{
+            item: "Apple"
+        }, {
+            item: "Orange"
+        }],
+        oCamparables: [
+          {
+            catId: 1,
+            catType: "Text",
+            catName: "Scientific",
+            catValues: [
+              "This is some text about Apple",
+              "This is some text about Orange"
+            ]
+          },
+          {
+            catId: 2,
+            catType: "OrderedList",
+            catName: "Nutrients",
+            catValues: [
+              ["Alpha", "Beta", "Gamma"],
+              ["Delta", "Theta", "Omega"]
+            ]
+          },
+          {
+            catId: 1,
+            catType: "UnOrderedList",
+            catName: "Physical",
+            catValues: [
+              ["0.5 lbs", "6 cms"],
+              ["0.76lbs", "5.3 cms"]
+            ]
+          }
+        ]
+    };
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    res.oShared = sharedProperties;
+    res.oService = {
+        GetCompare: function(id) {
+            //return CommonRoutines.FindItemInArray(res.arrResults, 'id', id, 'item');
+            return res.oCompair;
+        }
+    }
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    res.Helper = {
+        GetCompare: function(id) {
+            res.oShared.oCompair = res.oService.GetCompare(id);
+            res.oShared.ChangeStateTo('kompair.compare');
+        }
+    }
+}
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+function CompairCtrl(sharedProperties) {
+    var com = this;
+    //com.oShared = sharedProperties;
+    com.oCompair = sharedProperties.oCompair;
+}
+
+function MainCtrl($scope, sharedProperties) {
+    var main = this;
+    main.oShared = sharedProperties;
+
+    $scope.$on("$ionicView.beforeEnter", function(event, data) {
+        // handle event
+        //return;
+        console.log("State Params: ", data.stateId);
+        //main.showNavHeader = sharedProperties.bSingedIn;
+        //main.showNavHeader = data.stateId === "tabs.home" || data.stateId === "tabs.results" || data.stateId === "tabs.answer" || data.stateId === "tabs.logged_in_home" || data.stateId === "tabs.answer2" || data.stateId === "tabs.new" ? false : true;
+    });
+    $scope.$on("$ionicView.enter", function(event, data) {
+        // handle event
+        return;
+        console.log("State Params: ", data.stateId);
+    });
+    $scope.$on("$ionicView.afterEnter", function(event, data) {
+        // handle event
+        return;
+        console.log("State Params: ", data.stateId);
+    });
+};
---
+++ www/js/state.js
@@ -0,0 +1,63 @@
+kompair
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('kompair', {
+                url: "",
+                abstract: true,
+                templateUrl: "templates/base/tabs.html",
+                controller: 'HomeCtrl as vm'
+            })
+            .state('kompair.home', {
+                url: "/home",
+                views: {
+                    'home-tab': {
+                        templateUrl: "templates/base/home.html"
+                    }
+                }
+            })
+            .state('kompair.about', {
+                url: "/about",
+                views: {
+                    'about-tab': {
+                        templateUrl: "templates/base/about.html"
+                    }
+                }
+            })
+            .state('kompair.contact', {
+                url: "/contact",
+                views: {
+                    'contact-tab': {
+                        templateUrl: "templates/base/contact.html"
+                    }
+                }
+            })
+            .state('kompair.results', {
+                url: "/results",
+                views: {
+                    'home-tab': {
+                        templateUrl: "templates/kompair/results.html",
+                        controller: "ResultsCtrl as res"
+                    }
+                }
+            })
+            .state('kompair.compare', {
+                url: "/compare",
+                views: {
+                    'home-tab': {
+                        templateUrl: "templates/kompair/compare.html",
+                        controller: "CompairCtrl as com"
+                    }
+                }
+            })
+            .state('kompair.edit', {
+                url: "/edit",
+                views: {
+                    'home-tab': {
+                        templateUrl: "templates/kompair/edit.html"
+                    }
+                }
+            });
+
+
+        $urlRouterProvider.otherwise("/home");
+    })
